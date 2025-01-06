b1b5c2aa7ca37765310fa9e6e0f8870f0c076279
kubek22 <01171264@pw.edu.pl>
2025-01-06 23:04:52 +0100

    updating hbase script, updating nifi flow

--- scripts/define_hbase_tables.sh
+++ scripts/define_hbase_tables.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "create 'air_quality', 'm', 'g', 't'" | hbase shell
+echo "create 'air_quality', 'd'" | hbase shell
 echo "create 'airbnb', 'r', 'g'" | hbase shell
-echo "create 'property_sales', 'p', 'g'" | hbase shell
-echo "create 'geo', 'g'" | hbase shell
+echo "create 'property_sales', 'd'" | hbase shell
+echo "create 'geo', 'd'" | hbase shell
 
 
